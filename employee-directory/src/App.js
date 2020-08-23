29358ddd84e638ea31645341684ebc4b7c7db889
Aurora Sky <62398803+melissa-clothilde@users.noreply.github.com>
2020-08-22 17:17:26 -0700

    writes a component named EmployeeCard and renders employees to the page

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import EmployeeCard from './components/EmployeeCard';
 import employees from "./employees.json";
 
 class App extends Component {
   state = {
     employees
   };
-    
-  
-  render () {
+
+ // Map over this.state.wmployeews and render a EmployeeCard component for each employee object
+  render() {
     return (
-      <div>
-      <h1>Employee Directory</h1>
+      <div className='wrapper'>
+        <h1>Employee Directory</h1>
+        {this.state.employees.map(employee => (
+          <EmployeeCard
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            mage={employee.image}
+            occupation={employee.occupation}
+            location={employee.location}
+          />
+        ))}
       </div>
 
     )
